4714873f0384a8b3215e7481250509dff6453b33
HarishNarava <132235925+HarishNarava@users.noreply.github.com>
2023-12-11 22:04:42 -0500

    files upload

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import numpy as np
+import joblib
+from prediction import get_prediction, label_encoder
+
+# Load the pre-trained model
+model = joblib.load(r'Model/model.joblib')
+
+st.set_page_config(
+    page_title="Accident Severity Prediction App",
+    page_icon="🚧",
+    layout="wide"
+)
+
+# Define options and features
+options_day = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+options_type_of_collision = ['Collision with roadside-parked vehicles',
+                             'Vehicle with vehicle collision',
+                             'Collision with roadside objects', 'Collision with animals',
+                             'Other', 'Rollover', 'Fall from vehicles',
+                             'Collision with pedestrians', 'With Train']
+options_driving_exp = ['1-2yr', 'Above 10yr', '5-10yr', '2-5yr', "nan", 'No Licence', 'Below 1yr', 'unknown']
+options_road_surface = ['Dry', 'Wet or damp', 'Snow', 'Flood over 3cm. deep']
+options_types_of_junction = ['No junction', 'Y Shape', 'Crossing', 'O Shape', 'Other', 'Unknown', 'T Shape', 'X Shape']
+options_light_conditions = ['Daylight', 'Darkness - lights lit', 'Darkness - no lighting', 'Darkness - lights unlit']
+
+features = ['Minutes', 'Day_of_week', 'Number_of_vehicles_involved', 'Number_of_casualties', 'Light_conditions',
+            'Driving_experience', 'Road_surface_conditions', 'Types_of_Junction', 'Hour', 'Type_of_collision']
+
+st.markdown("<h1 style='text-align: center;'>Accident Severity Prediction App 🚧</h1>",
+            unsafe_allow_html=True)
+
+
+def main():
+    with st.form('prediction_form'):
+        st.subheader("Enter the input for the following features:")
+
+        col1, col2, col3 = st.columns(3)  # Create 3 columns for input widgets
+        with col1:
+            Hour = st.slider("Pickup Hour: ", 0, 23, value=0, format="%d")
+            Minutes = st.slider("Pickup Minutes: ", 0, 59, value=0, format="%d")
+            Day_of_week = st.selectbox("Select Day of the Week: ", options=options_day)
+        with col2:
+            Number_of_casualties = st.slider("Number of Casualties: ", 0, 7, value=0, format="%d")
+            Number_of_vehicles_involved = st.slider("Number of Vehicles Involved: ", 0, 7, value=0, format="%d")
+            Light_conditions = st.selectbox("Select Lighting Conditions: ", options=options_light_conditions)
+        with col3:
+            Driving_experience = st.selectbox("Select Driving Experience of the Driver: ", options=options_driving_exp)
+            Road_surface_conditions = st.selectbox("Select Surface of the Road: ", options=options_road_surface)
+            Types_of_Junction = st.selectbox("Select the type of Junction: ", options=options_types_of_junction)
+            Type_of_collision = st.selectbox("Select the type of Collision: ", options=options_type_of_collision)
+
+        submit = st.form_submit_button("Predict")
+
+    if submit:
+        Day_of_week = label_encoder(Day_of_week, options_day)
+        Driving_experience = label_encoder(Driving_experience, options_driving_exp)
+        Road_surface_conditions = label_encoder(Road_surface_conditions, options_road_surface)
+        Types_of_Junction = label_encoder(Types_of_Junction, options_types_of_junction)
+        Type_of_collision = label_encoder(Type_of_collision, options_type_of_collision)
+        Light_conditions = label_encoder(Light_conditions, options_light_conditions)
+
+        # Prepare the input data
+        data = np.array([Minutes, Day_of_week, Number_of_vehicles_involved, Number_of_casualties, Light_conditions,
+                         Driving_experience, Road_surface_conditions, Types_of_Junction, Hour, Type_of_collision]).reshape(1, -1)
+
+        # Get the prediction
+        pred = get_prediction(data=data, model=model)
+
+        st.write(f"The predicted severity is:  {pred[0]}")
+
+
+if __name__ == '__main__':
+    main()
